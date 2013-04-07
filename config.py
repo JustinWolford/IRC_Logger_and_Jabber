53572f8d192aa918774485bbe0dcd26d46d2886c
Justin W <Nope@nada.com>
2013-04-06 21:23:11 -0700

    Fixed indentation

--- config.py
+++ config.py
@@ -2,20 +2,20 @@ import ast
 
 
 def config(filename):
-    """ Process a file (located at filename)
-        containing a series of key-value pairs
-        this: awesome
-        numkids: 10
-        funk: soulbrother
-        Into a dictionary. (dict[this] = "awesome")
-    """
-    try:
-        config_data = dict()
-        db_file = open(filename, "r")
-        for line in db_file:
-            if line.startswith("#"):
-                continue
-            temp = line.replace(":", "").split()
+	""" Process a file (located at filename)
+		containing a series of key-value pairs
+		this: awesome
+		numkids: 10
+		funk: soulbrother
+		Into a dictionary. (dict[this] = "awesome")
+	"""
+	try:
+		config_data = dict()
+		db_file = open(filename, "r")
+		for line in db_file:
+			if line.startswith("#"):
+				continue
+			temp = line.replace(":", "").split()
 			if temp[0] == "alert":
 				key = temp[0]
 				value = ast.eval(join(temp[1:]))
@@ -23,13 +23,13 @@ def config(filename):
 				key = temp[0]
 				value = temp[1:]
 				config_data[key] = value
-        db_file.close()
-        return config_data
-    except:
-        print filename, "missing."
-        exit();
+		db_file.close()
+		return config_data
+	except:
+		print filename, "missing."
+		exit();
 
 if __name__ == "__main__":
-    irc_config = config("mysql_config.txt")
-    for key,value in irc_config.iteritems():
-        print key, value
+	irc_config = config("mysql_config.txt")
+	for key,value in irc_config.iteritems():
+		print key, value
